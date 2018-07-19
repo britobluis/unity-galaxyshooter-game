0c7c7e727b296c6d0a6491eff039a81eab501b64
Luis Brito <nottluis@gmail.com>
2018-07-19 18:57:02 -0300

    Initial commit with player movement implemented

---
+++ Galaxy Shooter/Assets/Galaxy Shooter/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    //public or private identify
+    //data type (int, float, bool, strings)
+    //every variable has a NAME
+    //optional value assigned
+
+    [SerializeField]
+    private float speed = 10.0f;
+
+
+	// Use this for initialization
+	void Start () {
+        // set new position
+        transform.position = new Vector3(0, 0, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movement();
+    }
+
+    private void movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
+
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        }
+        else if (transform.position.y < -4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.2f, 0);
+        }
+
+        if (transform.position.x > 9.4f)
+        {
+            transform.position = new Vector3(-9.4f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -9.4f)
+        {
+            transform.position = new Vector3(9.4f, transform.position.y, 0);
+        }
+    }
+}
